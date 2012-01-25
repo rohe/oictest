6f170971715a25015fb7f47a41f3ae0f82043209
Roland Hedberg <roland.hedberg@adm.umu.se>
2012-01-25 11:03:05 +0100

    Additional dependencies

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     classifiers = ["Development Status :: 4 - Beta",
                    "License :: OSI Approved :: Apache Software License",
                    "Topic :: Software Development :: Libraries :: Python Modules"],
-    install_requires = ["oic", "mechanize"],
+    install_requires = ["oic", "mechanize", "argparse", "import_lib"],
 
     zip_safe=False,
     scripts=["script/oauth2c.py", "script/oicc.py"],
