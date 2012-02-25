bd0ed2354bb73b90ad1ee94e45d1e4160f0b8ff9
nov matake <nov@matake.jp>
2012-02-25 15:44:21 +0900

    copy&paste bug.

--- op_config/nov.py
+++ op_config/nov.py
@@ -12,17 +12,17 @@ info = {
   },
   "provider": {
     "version": "3.0",
-    "issuer": "https://connectop.heroku.com",
-    "authorization_endpoint": "https://connectop.heroku.com/authorizations/new",
-    "token_endpoint": "https://connectop.heroku.com/access_tokens",
-    "userinfo_endpoint": "https://connectop.heroku.com/user_info",
-    "check_id_endpoint": "https://connectop.heroku.com/id_token",
-    "registration_endpoint": "https://connectop.heroku.com/connect/client",
+    "issuer": "https://connect-op.heroku.com",
+    "authorization_endpoint": "https://connect-op.heroku.com/authorizations/new",
+    "token_endpoint": "https://connect-op.heroku.com/access_tokens",
+    "userinfo_endpoint": "https://connect-op.heroku.com/user_info",
+    "check_id_endpoint": "https://connect-op.heroku.com/id_token",
+    "registration_endpoint": "https://connect-op.heroku.com/connect/client",
     "scopes_supported": ["openid", "profile", "email", "address", "phone"],
     "response_types_supported": ["code", "token", "id_token", "code token", "code id_token", "id_token token", "code id_token token"],
     "user_id_types_supported": ["public", "pairwise"],
     "id_token_algs_supported": ["RS256"],
-    "x509_url": "https://connectop.heroku.com/cert.pem"
+    "x509_url": "https://connect-op.heroku.com/cert.pem"
   },
   "interaction": {
     "https://connect-op.heroku.com/": [
