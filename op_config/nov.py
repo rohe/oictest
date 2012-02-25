b5d1ef104f643fcdf75e7598d01cf92102410179
nov matake <nov@matake.jp>
2012-02-25 15:42:04 +0900

    hum, this seems needed to tell oictest to use RSA as ID Token signature algorithm

--- op_config/nov.py
+++ op_config/nov.py
@@ -11,11 +11,18 @@ info = {
     "register": True
   },
   "provider": {
-      "version": {
-          "oauth": "2.0",
-          "openid": "3.0"
-      },
-      "dynamic": "https://connect-op.heroku.com/"
+    "version": "3.0",
+    "issuer": "https://connectop.heroku.com",
+    "authorization_endpoint": "https://connectop.heroku.com/authorizations/new",
+    "token_endpoint": "https://connectop.heroku.com/access_tokens",
+    "userinfo_endpoint": "https://connectop.heroku.com/user_info",
+    "check_id_endpoint": "https://connectop.heroku.com/id_token",
+    "registration_endpoint": "https://connectop.heroku.com/connect/client",
+    "scopes_supported": ["openid", "profile", "email", "address", "phone"],
+    "response_types_supported": ["code", "token", "id_token", "code token", "code id_token", "id_token token", "code id_token token"],
+    "user_id_types_supported": ["public", "pairwise"],
+    "id_token_algs_supported": ["RS256"],
+    "x509_url": "https://connectop.heroku.com/cert.pem"
   },
   "interaction": {
     "https://connect-op.heroku.com/": [
