8471f1bf0ce87cb38173d16a27f4e4e353f0ccdf
nov matake <nov@matake.jp>
2012-02-25 15:40:32 +0900

    nov, ryo and edmund config

--- op_config/nov.py
+++ op_config/nov.py
@@ -11,23 +11,15 @@ info = {
     "register": True
   },
   "provider": {
-    "version": "3.0",
-    "issuer": "https://connect-op.heroku.com",
-    "authorization_endpoint": "https://connect-op.heroku.com/authorizations/new",
-    "token_endpoint": "https://connect-op.heroku.com/access_tokens",
-    "userinfo_endpoint": "https://connect-op.heroku.com/user_info",
-    "check_id_endpoint": "https://connect-op.heroku.com/id_token",
-    "registration_endpoint": "https://connect-op.heroku.com/connect/client",
-    "scopes_supported": ["openid", "profile", "email", "address", "phone"],
-    "response_types_supported": ["code", "token", "id_token", "code token", "code id_token", "id_token token", "code id_token token"],
-    "user_id_types_supported": ["public", "pairwise"],
-    "id_token_algs_supported": ["RS256"],
-    "x509_url": "https://connect-op.heroku.com/cert.pem"
+      "version": {
+          "oauth": "2.0",
+          "openid": "3.0"
+      },
+      "dynamic": "https://connect-op.heroku.com/"
   },
   "interaction": {
     "https://connect-op.heroku.com/": [
-      "select_form",
-      {
+      "select_form", {
         "_form_pick_": {
           "action": "/connect/fake"
         }
