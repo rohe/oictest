0d73846ce8d596ed7a50137bfbcf9aadf1ff2987
nov matake <nov@matake.jp>
2012-02-25 15:42:49 +0900

    spaces in JSON

--- op_config/ryo.py
+++ op_config/ryo.py
@@ -3,24 +3,24 @@
 import json
 
 info = {
-    "client": {
-        "redirect_uris": ["https://smultron.catalogix.se/authz_cb"],
-        "contact": ["roland.hedberg@adm.umu.se"],
-        "application_type": "web",
-        "application_name": "OIC test tool",
-        "register": True
+  "client": {
+    "redirect_uris": ["https://smultron.catalogix.se/authz_cb"],
+    "contact": ["roland.hedberg@adm.umu.se"],
+    "application_type": "web",
+    "application_name": "OIC test tool",
+    "register": True
+  },
+  "provider": {
+    "version": {
+      "oauth": "2.0",
+      "openid": "3.0"
     },
-    "provider": {
-        "version": {
-            "oauth": "2.0",
-            "openid": "3.0"
-        },
-        "dynamic": "https://openidconnect.info/"
-    },
-    "interaction": {
-        "https://openidconnect.info/account/login": ["chose", {"path": "/account/fake"}],
-        "https://openidconnect.info/connect/consent": ["select_form", {}]
-    }
+    "dynamic": "https://openidconnect.info/"
+  },
+  "interaction": {
+    "https://openidconnect.info/account/login": ["chose", {"path": "/account/fake"}],
+    "https://openidconnect.info/connect/consent": ["select_form", {}]
+  }
 }
 
 print json.dumps(info)
