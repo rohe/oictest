59d250ca54b3a818aad36a0ab2a69d9e6a89f1d8
Roland Hedberg <roland.hedberg@adm.umu.se>
2012-09-13 12:48:28 +0200

    Updated test configurations

---
+++ test/oic_op/localhost.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import json
+
+info = {
+    "features": {
+        "registration":True,
+        "discovery": True,
+        "session_management": False,
+        "key_export": True,
+        "use_nonce": True,
+        "sector_identifier_url": True
+    },
+    "client": {
+        "redirect_uris": ["https://%s/authz_cb"],
+        "contact": ["roland.hedberg@adm.umu.se"],
+        "application_type": "web",
+        "application_name": "OIC test tool",
+        "key_export_url": "http://%s:8090/",
+        "preferences":{
+            "user_id_type": ["pairwise", "public"],
+            "require_signed_request_object": ["RS256", "RS384", "RS512",
+                                              "HS512", "HS384", "HS256"],
+            "token_endpoint_auth_type": ["client_secret_basic",
+                                         "client_secret_post",
+                                         "client_secret_jwt",
+                                         "private_key_jwt"],
+            "userinfo_signed_response_alg": ["RS256", "RS384", "RS512",
+                                              "HS512", "HS384", "HS256"],
+            #"userinfo_encrypted_response_alg",
+            #"userinfo_encrypted_response_enc",
+            #"userinfo_encrypted_response_int",
+            "id_token_signed_response_alg": ["RS256", "RS384", "RS512",
+                                              "HS512", "HS384", "HS256"],
+            #"id_token_encrypted_response_alg",
+            #"id_token_encrypted_response_enc",
+            #"id_token_encrypted_response_int",
+            "default_max_age": 3600,
+            "require_auth_time": True,
+            "default_acr":["2", "1"]
+        }
+
+    },
+    "provider": {
+        "version": { "oauth": "2.0", "openid": "3.0"},
+        "dynamic": "https://localhost:8088/",
+        },
+    "interaction": [
+            {
+            "matches": {
+                "url": "https://localhost:8088/authorization",
+                },
+            "page-type": "login",
+            "control": {
+                "type": "form",
+                "set": {"login":"diana","password": "krall"}
+            }
+        }
+    ]
+}
+
+print json.dumps(info)
