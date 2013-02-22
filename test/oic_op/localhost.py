e094e1f643cd2dffcfc57af860200cdcfd25a532
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-02-22 14:36:37 +0100

    Added a configuration file for localhost server.

---
+++ test/oic_op/localhost.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import json
+
+from default import DEFAULT
+
+info = DEFAULT.copy()
+
+info["provider"] = {"dynamic": "https://localhost:8092/"}
+
+info["interaction"] =[
+            {
+            "matches": {
+                "url": "https://localhost:8092/authorization",
+                },
+            "page-type": "login",
+            "control": {
+                "type": "form",
+                "set": {"login":"diana","password": "krall"}
+            }
+        }
+    ]
+
+print json.dumps(info)
