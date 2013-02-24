25bb8739b63eebbf49b3c0091a56c6fb5e4bcc33
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-02-24 17:09:50 +0100

    Broke out OAuth2 stuff into a branch of its own.

---
+++ src/oauth2test/__init__.py
@@ -0,0 +1,296 @@
+import json
+import argparse
+import sys
+from oauth2test.base import Conversation
+from oic.utils import exception_trace
+from rrtest import Trace, FatalError
+
+__author__ = 'rolandh'
+
+
+def flow2sequence(operations, item):
+    flow = operations.FLOWS[item]
+    return [operations.PHASES[phase] for phase in flow["sequence"]]
+
+
+class OAuth2(object):
+    client_args = ["client_id", "redirect_uris", "password", "client_secret"]
+
+    def __init__(self, operations_mod, client_class, msg_factory, chk_factory):
+        self.operations_mod = operations_mod
+        self.client_class = client_class
+        self.client = None
+        self.trace = Trace()
+        self.msg_factory = msg_factory
+        self.chk_factory = chk_factory
+
+        self._parser = argparse.ArgumentParser()
+        #self._parser.add_argument('-v', dest='verbose', action='store_true')
+        self._parser.add_argument('-d', dest='debug', action='store_true',
+                                  help="Print debug information")
+        self._parser.add_argument('-v', dest='verbose', action='store_true',
+                                  help="Print runtime information")
+        self._parser.add_argument(
+            '-C', dest="ca_certs",
+            help="CA certs to use to verify HTTPS server certificates, if HTTPS is used and no server CA certs are defined then no cert verification is done")
+        self._parser.add_argument('-J', dest="json_config_file",
+                                  help="Script configuration")
+        self._parser.add_argument('-I', dest="interactions",
+                                  help="Extra interactions not defined in the script configuration file")
+        self._parser.add_argument("-l", dest="list", action="store_true",
+                                  help="List all the test flows as a JSON object")
+        self._parser.add_argument("-H", dest="host",
+                                  help="Which host the script is running on, used to construct the key export URL")
+        self._parser.add_argument("flow", nargs="?",
+                                  help="Which test flow to run")
+
+        self.args = None
+        self.pinfo = None
+        self.sequences = []
+        self.function_args = {}
+        self.signing_key = None
+        self.encryption_key = None
+        self.test_log = []
+        self.environ = {}
+        self._pop = None
+
+    def parse_args(self):
+        self.json_config = self.json_config_file()
+
+        try:
+            self.features = self.json_config["features"]
+        except KeyError:
+            self.features = {}
+
+        self.pinfo = self.provider_info()
+
+        self.client_conf(self.client_args)
+
+    def json_config_file(self):
+        if self.args.json_config_file == "-":
+            return json.loads(sys.stdin.read())
+        else:
+            return json.loads(open(self.args.json_config_file).read())
+
+    def provider_info(self):
+        # Should provide a Metadata class
+        res = {}
+        _jc = self.json_config["provider"]
+
+        # Backward compatible
+        if "endpoints" in _jc:
+            try:
+                for endp, url in _jc["endpoints"].items():
+                    res[endp] = url
+            except KeyError:
+                pass
+
+        return res
+
+    def test_summation(self, sid):
+        status = 0
+        for item in self.test_log:
+            if item["status"] > status:
+                status = item["status"]
+
+        if status == 0:
+            status = 1
+
+        info = {
+            "id": sid,
+            "status": status,
+            "tests": self.test_log
+        }
+
+        if status == 5:
+            info["url"] = self.test_log[-1]["url"]
+            info["htmlbody"] = self.test_log[-1]["message"]
+
+        return info
+
+    def run(self):
+        self.args = self._parser.parse_args()
+
+        if self.args.list:
+            return self.operations()
+        else:
+            if not self.args.flow:
+                raise Exception("Missing flow specification")
+            self.args.flow = self.args.flow.strip("'")
+            self.args.flow = self.args.flow.strip('"')
+
+            flow_spec = self.operations_mod.FLOWS[self.args.flow]
+            try:
+                block = flow_spec["block"]
+            except KeyError:
+                block = {}
+
+            self.parse_args()
+            _spec = self.make_sequence()
+            interact = self.get_interactions()
+
+            try:
+                self.do_features(interact, _spec["sequence"], block)
+            except Exception, exc:
+                exception_trace("do_features", exc)
+                return
+
+            #tests = self.get_test()
+            self.client.state = "STATE0"
+
+            try:
+                except_exception = flow_spec["except_exception"]
+            except KeyError:
+                except_exception = False
+
+            try:
+                if self.args.verbose:
+                    print >> sys.stderr, "Set up done, running sequence"
+                conv = Conversation(self.client, self.cconf, self.trace,
+                                    interact, msg_factory=self.msg_factory,
+                                    check_factory=self.chk_factory)
+                conv.do_sequence(_spec)
+                #testres, trace = do_sequence(oper,
+                self.test_log = conv.test_output
+                tsum = self.test_summation(self.args.flow)
+                print >>sys.stdout, json.dumps(tsum)
+                if tsum["status"] > 1 or self.args.debug:
+                    print >> sys.stderr, self.trace
+            except FatalError, err:
+                print >> sys.stderr, self.trace
+                print err
+                #exception_trace("RUN", err)
+            except Exception, err:
+                print >> sys.stderr, self.trace
+                print err
+                exception_trace("RUN", err)
+
+            #if self._pop is not None:
+            #    self._pop.terminate()
+            if "keyprovider" in self.environ and self.environ["keyprovider"]:
+                # os.kill(self.environ["keyprovider"].pid, signal.SIGTERM)
+                self.environ["keyprovider"].terminate()
+
+    def operations(self):
+        lista = []
+        for key, val in self.operations_mod.FLOWS.items():
+            item = {"id": key, "name": val["name"]}
+            try:
+                _desc = val["descr"]
+                if isinstance(_desc, basestring):
+                    item["descr"] = _desc
+                else:
+                    item["descr"] = "\n".join(_desc)
+            except KeyError:
+                pass
+
+            for key in ["depends", "endpoints"]:
+                try:
+                    item[key] = val[key]
+                except KeyError:
+                    pass
+
+            lista.append(item)
+
+        print json.dumps(lista)
+
+    def do_features(self, *args):
+        pass
+
+    def export(self):
+        pass
+
+    def client_conf(self, cprop):
+        if self.args.ca_certs:
+            self.client = self.client_class(ca_certs=self.args.ca_certs)
+        else:
+            try:
+                self.client = self.client_class(
+                    ca_certs=self.json_config["ca_certs"])
+            except (KeyError, TypeError):
+                self.client = self.client_class()
+
+        #self.client.http_request = self.client.http.crequest
+
+        # set the endpoints in the Client from the provider information
+        # If they are statically configured, if dynamic it happens elsewhere
+        for key, val in self.pinfo.items():
+            if key.endswith("_endpoint"):
+                setattr(self.client, key, val)
+
+        # Client configuration
+        self.cconf = self.json_config["client"]
+        # replace pattern with real value
+        _h = self.args.host
+        if _h:
+            self.cconf["redirect_uris"] = [p % _h for p in
+                                           self.cconf["redirect_uris"]]
+
+        try:
+            self.client.client_prefs = self.cconf["preferences"]
+        except KeyError:
+            pass
+
+        # set necessary information in the Client
+        for prop in cprop:
+            try:
+                setattr(self.client, prop, self.cconf[prop])
+            except KeyError:
+                pass
+
+    def make_sequence(self):
+        # Whatever is specified on the command line takes precedences
+        if self.args.flow:
+            sequence = flow2sequence(self.operations_mod, self.args.flow)
+            _flow = self.args.flow
+        elif self.json_config and "flow" in self.json_config:
+            sequence = flow2sequence(self.operations_mod,
+                                     self.json_config["flow"])
+            _flow = self.json_config["flow"]
+        else:
+            sequence = None
+            _flow = ""
+
+        res = {"sequence": sequence, "tests": {"pre": [],"post": []}}
+
+        if _flow:
+            try:
+                res["tests"]["post"] = self.operations_mod.FLOWS[_flow]["tests"]
+            except KeyError:
+                pass
+
+        return res
+
+    def get_interactions(self):
+        interactions = []
+
+        if self.json_config:
+            try:
+                interactions = self.json_config["interaction"]
+            except KeyError:
+                pass
+
+        if self.args.interactions:
+            _int = self.args.interactions.replace("\'", '"')
+            if interactions:
+                interactions.update(json.loads(_int))
+            else:
+                interactions = json.loads(_int)
+
+        return interactions
+
+    def get_test(self):
+        if self.args.flow:
+            flow = self.operations_mod.FLOWS[self.args.flow]
+        elif self.json_config and "flow" in self.json_config:
+            flow = self.operations_mod.FLOWS[self.json_config["flow"]]
+        else:
+            flow = None
+
+        try:
+            return flow["tests"]
+        except KeyError:
+            return []
+
+    def register_args(self):
+        pass
---
+++ src/oauth2test/base.py
@@ -0,0 +1,177 @@
+from oic.oauth2 import Message
+import sys
+from rrtest import tool, FatalError
+from rrtest.interaction import Operation
+
+__author__ = 'rolandh'
+
+
+class Conversation(tool.Conversation):
+    def __init__(self, client, config, trace, interaction, msg_factory,
+                 check_factory, features=None, verbose=False):
+        tool.Conversation.__init__(self, client, config, trace,
+                                   interaction, check_factory, msg_factory,
+                                   features, verbose)
+        self.cis = []
+        self.protocol_response = []
+        #self.item = []
+        #self.keyjar = self.client.keyjar
+        self.position = ""
+        self.last_response = None
+        self.last_content = None
+        self.accept_exception = False
+        self.creq = None
+        self.cresp = None
+        self.msg_factory = msg_factory
+        self.login_page = None
+        self.response_message = None
+
+    def init(self, phase):
+        self.creq, self.cresp = phase
+
+    def my_endpoints(self):
+        return self.client.redirect_uris
+
+    def do_response(self, resp, resp_type):
+        if isinstance(resp.response, basestring):
+            response = self.msg_factory(resp.response)
+        else:
+            response = resp.response
+
+        self.response_type = response.__name__
+        try:
+            _qresp = self.client.parse_response(
+                response, self.info, resp_type, self.client.state,
+                client_id=self.client.client_id)
+            self.trace.info("[%s]: %s" % (_qresp.type(), _qresp.to_dict()))
+            #item.append(qresp)
+            self.response_message = _qresp
+            self.protocol_response.append((_qresp, self.info))
+            err = None
+        except Exception, err:
+            self.exception = "%s" % err
+
+        if err and self.accept_exception:
+            if isinstance(err, self.accept_exception):
+                self.trace.info("Got expected exception: %s [%s]" % (
+                    err, err.__class__.__name__))
+            else:
+                raise
+        else:
+            self.do_check("response-parse")
+        
+    def post_process(self, resp):
+        if self.response_message:
+            try:
+                self.test_sequence(resp.tests["post"])
+            except KeyError:
+                pass
+
+            resp(self, self.response_message)
+
+            return True
+        else:
+            return False
+    
+    def handle_result(self):
+        try:
+            self.response_spec = resp = self.cresp()
+        except TypeError:
+            self.response_spec = resp = None
+            return True
+
+        _qresp = None
+        self.info = None
+
+        response = self.last_response
+        resp_type = resp.ctype
+        if response:
+            try:
+                ctype = response.headers["content-type"]
+                if ctype == "application/jwt":
+                    resp_type = "jwt"
+            except (AttributeError, TypeError):
+                pass
+
+        if response.status_code >= 400:
+            pass
+        elif not self.position:
+            if isinstance(self.last_content, Message):
+                self.response_message = self.last_content
+            elif response.status_code == 200:
+                self.info = self.last_content
+        elif resp.where == "url" or response.status_code == 302:
+            try:
+                self.info = response.headers["location"]
+                resp_type = "urlencoded"
+            except KeyError:
+                try:
+                    _check = getattr(self.creq, "interaction_check", None)
+                except AttributeError:
+                    _check = None
+
+                if _check:
+                    self.err_check("interaction-check")
+                else:
+                    self.do_check("missing-redirect")
+        else:
+            self.do_check("check_content_type_header")
+            self.info = self.last_content
+
+        if self.info and resp.response:
+            self.do_response(resp, resp_type)
+            
+        self.post_process(resp)
+        
+    def setup_request(self):
+        self.request_spec = req = self.creq(conv=self)
+
+        if isinstance(req, Operation):
+            for intact in self.interaction.interactions:
+                try:
+                    if req.__class__.__name__ == intact["matches"]["class"]:
+                        req.args = intact["args"]
+                        break
+                except KeyError:
+                    pass
+        else:
+            try:
+                self.request_args = req.request_args
+            except KeyError:
+                pass
+            try:
+                self.args = req.kw_args
+            except KeyError:
+                pass
+
+        # The authorization dance is all done through the browser
+        if req.request == "AuthorizationRequest":
+            self.client.cookiejar = self.cjar["browser"]
+        # everything else by someone else, assuming the RP
+        else:
+            self.client.cookiejar = self.cjar["rp"]
+
+        self.req = req
+
+    def send(self):
+        try:
+            self.test_sequence(self.req.tests["pre"])
+        except KeyError:
+            pass
+
+        try:
+            if self.verbose:
+                print >> sys.stderr, "> %s" % self.req.request
+            part = self.req(self.position, self.last_response,
+                            self.last_content, self.features)
+            (self.position, self.last_response, self.last_content) = part
+
+            try:
+                if not self.test_sequence(self.req.tests["post"]):
+                    self.position = None
+            except KeyError:
+                pass
+        except FatalError:
+            raise
+        except Exception, err:
+            self.err_check("exception", err)
