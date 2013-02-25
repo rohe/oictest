16d538c36f1100c7bbd97f740ae2481ddc4eb968
Emil Eriksson <emil.eriksson@codemill.se>
2013-02-25 15:34:24 +0100

    Clearer parameters in client-scripts
    Uses longer names for the parameters in the tests-scripts closer to the
    recommendations of PEP8.

--- script/oauth2c.py
+++ script/oauth2c.py
@@ -3,13 +3,13 @@
 __author__ = 'rohe0002'
 
 from oic.oauth2 import Client
-from oic.oauth2 import factory
+from oic.oauth2 import factory as message_factory
 from oauth2test import OAuth2
 from oauth2test import oauth2_operations
 
 from oauth2test.base import Conversation
-from oauth2test.check import factory as chk_factory
+from oauth2test.check import factory as check_factory
 
-cli = OAuth2(oauth2_operations, Client, factory, chk_factory, Conversation)
+cli = OAuth2(oauth2_operations, Client, message_factory, check_factory, Conversation)
 
 cli.run()
--- script/oicc.py
+++ script/oicc.py
@@ -5,12 +5,12 @@ __author__ = 'rohe0002'
 from oictest import OIC
 from oictest import Conversation
 from oictest import oic_operations
-from oictest.check import factory as chk_factory
+from oictest.check import factory as check_factory
 
 from oic.oic import Client
 from oic.oic.consumer import Consumer
-from oic.oic.message import factory
+from oic.oic.message import factory as message_factory
 
-cli = OIC(oic_operations, Client, Consumer, factory, chk_factory, Conversation)
+cli = OIC(oic_operations, Client, Consumer, message_factory, check_factory, Conversation)
 
 cli.run()
