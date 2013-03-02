023144c4bc3260e11fabf6926746782e564c6ca3
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-03-02 10:12:03 +0100

    Import the class directly and not indirectly.

--- script/oicc.py
+++ script/oicc.py
@@ -3,14 +3,15 @@
 __author__ = 'rohe0002'
 
 from oictest import OIC
-from oictest import Conversation
 from oictest import oic_operations
+from oictest.base import Conversation
 from oictest.check import factory as check_factory
 
 from oic.oic import Client
 from oic.oic.consumer import Consumer
 from oic.oic.message import factory as message_factory
 
-cli = OIC(oic_operations, Client, Consumer, message_factory, check_factory, Conversation)
+cli = OIC(oic_operations, Client, Consumer, message_factory, check_factory,
+          Conversation)
 
 cli.run()
