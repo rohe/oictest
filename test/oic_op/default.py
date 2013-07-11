7a2df27c515a255e48e5763027c85e6a64fad45f
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-07-11 10:33:41 -0700

    Fixed a couple of faulty parameter names. Added response_types and grant_types.

--- test/oic_op/default.py
+++ test/oic_op/default.py
@@ -11,9 +11,9 @@ DEFAULT = {
     },
     "client": {
         "redirect_uris": ["https://%s/authz_cb"],
-        "contact": ["roland.hedberg@adm.umu.se"],
+        "contacts": ["roland.hedberg@adm.umu.se"],
         "application_type": "web",
-        "application_name": "OIC test tool",
+        "client_name": "OIC test tool",
         "key_export_url": "http://%s:8090/",
         "keys": {
             "RSA": {
@@ -25,25 +25,34 @@ DEFAULT = {
         #"userinfo_signed_response_alg": "RS256",
         #"id_token_signed_response_alg": "RS256"
         "preferences":{
-            "subject_type": ["pairwise", "public"],
-            "request_object_signing_alg": ["RS256", "RS384", "RS512",
-                                           "HS512", "HS384", "HS256"],
-            "token_endpoint_auth_methods_supported": [
+            "subject_types": ["pairwise", "public"],
+            "request_object_signing_algs": [
+                "RS256", "RS384", "RS512", "HS512", "HS384", "HS256"
+            ],
+            "token_endpoint_auth_methods": [
                 "client_secret_basic", "client_secret_post",
                 "client_secret_jwt", "private_key_jwt"],
-            #"userinfo_signed_response_algs": ["RS256", "RS384", "RS512",
-            #                                  "HS512", "HS384", "HS256"],
+            "response_types": [
+                "code", "token", "id_token", "token id_token",
+                "code id_token", "code token", "code token id_token"
+            ],
+            "grant_types":["authorization_code", "implicit", "refresh_token",
+                           "urn:ietf:params:oauth:grant-type:jwt-bearer:"],
+            "userinfo_signed_response_algs": [
+                "RS256", "RS384", "RS512", "HS512", "HS384", "HS256"
+            ],
             #"userinfo_encrypted_response_alg",
             #"userinfo_encrypted_response_enc",
             #"userinfo_encrypted_response_int",
-            "id_token_signed_response_alg": [
-                "RS256", "RS384", "RS512", "HS512", "HS384", "HS256"],
+            "id_token_signed_response_algs": [
+                "RS256", "RS384", "RS512", "HS512", "HS384", "HS256"
+            ],
             #"id_token_encrypted_response_alg",
             #"id_token_encrypted_response_enc",
             #"id_token_encrypted_response_int",
             "default_max_age": 3600,
             "require_auth_time": True,
-            "default_acr":["2", "1"]
+            "default_acr_values":["2", "1"]
         }
     },
 }
