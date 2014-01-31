3a32ffa6ba6695977c830f5ac3a6110b4827b22a
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-01-31 09:45:08 +0100

    New year

--- script/static_provider.py
+++ script/static_provider.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # Just for serving static files
-import socket
 
 __author__ = 'rohe0002'
 
@@ -9,14 +8,17 @@ import SocketServer
 
 
 class MyTCPServer(SocketServer.TCPServer):
-    def __init__(self, server_address, RequestHandlerClass):
+    def __init__(self, server_address, requesthandlerclass):
         self.allow_reuse_address = True
         SocketServer.TCPServer.__init__(self, server_address,
-                                        RequestHandlerClass)
+                                        requesthandlerclass)
+
+    def __call__(self, *args, **kwargs):
+        return "MyTCPServer"
 
 
-def main(arg):
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+def main(arg=None):
+    handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
     if arg[0] == "localhost":
         hostname = ""
@@ -27,14 +29,11 @@ def main(arg):
 #    s = socket.socket()
 #    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 #    s.bind((hostname, port))
-    httpd = MyTCPServer((hostname, port), Handler)
+    httpd = MyTCPServer((hostname, port), handler)
     print "Starting request handler on %s:%s" % (hostname, arg[1])
     httpd.serve_forever()
 
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) > 1:
-        main(sys.argv[1:])
-    else:
-        main()
+    main(sys.argv[1:])
