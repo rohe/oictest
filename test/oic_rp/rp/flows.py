33b55f2740286d787dd3bbd49080f58387fd900a
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-12-09 13:39:58 +0100

    Changed naming so sorting works.

--- test/oic_rp/rp/flows.py
+++ test/oic_rp/rp/flows.py
@@ -4,44 +4,44 @@ __author__ = 'roland'
 MODE = {}
 
 FLOWS = {
-    "RP-1": {
+    "RP-01": {
         "flow": [("discover", None)],
         "desc": "Can Discover Identifiers using URL Syntax"
     },
-    "RP-2": {
+    "RP-02": {
         "flow": [("discover", "acct:local@localhost:8080")],
         "desc": "Can Discover Identifiers using acct Syntax"
     },
-    "RP-3": {
+    "RP-03": {
         "flow": [("discover", None), ("provider_info", None)],
         "desc": "Uses openid-configuration Discovery Information"
     },
-    "RP-4": {
+    "RP-04": {
         "flow": [("discover", None), ("provider_info", None),
                  ("registration", None)],
         "desc": "Uses Dynamic Registration"
     },
-    "RP-5": {
+    "RP-05": {
         "flow": [("discover", None), ("provider_info", None),
                  ("registration", None),
                  ("authn_req", {"scope": "openid", "response_type": ["code"]})],
         "desc": "Can Make Request with 'code' Response Type"
     },
-    "RP-6": {
+    "RP-06": {
         "flow": [("discover", None), ("provider_info", None),
                  ("registration", None),
                  ("authn_req", {"scope": "openid",
                                 "response_type": ["id_token"]})],
         "desc": "Can Make Request with 'id_token' Response Type"
     },
-    "RP-7": {
+    "RP-07": {
         "flow": [("discover", None), ("provider_info", None),
                  ("registration", None),
                  ("authn_req", {"scope": "openid",
                                 "response_type": ["id_token", "token"]})],
         "desc": "Can Make Request with 'id_token token' Response Type"
     },
-    "RP-8": {
+    "RP-08": {
         "flow": [("discover", None), ("provider_info", None),
                  ("registration", None),
                  ("authn_req", {"scope": "openid",
@@ -50,7 +50,7 @@ FLOWS = {
         "desc": "Can Make Access Token Request with 'client_secret_basic' "
                 "Authentication"
     },
-    "RP-9": {
+    "RP-09": {
         "flow": [("discover", None), ("provider_info", None),
                  ("registration", None),
                  ("authn_req", {"scope": "openid",
@@ -73,8 +73,8 @@ FLOWS = {
                  ("registration", None),
                  ("authn_req", {"scope": "openid",
                                 "response_type": ["code"]}),
-                 ("token_req", {"authn_method": "client_secret_basic"})],
-        "desc": "Can Make Access Token Request with 'client_secret_basic' "
+                 ("token_req", {"authn_method": "private_key_jwt"})],
+        "desc": "Can Make Access Token Request with 'private_key_jwt' "
                 "Authentication"
     },
 }
