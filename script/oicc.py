cda6db98021ccaaa9db684cab76a190216438e7f
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-12-16 12:16:03 +0100

    Added logger instance to oicc

--- script/oicc.py
+++ script/oicc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import logging
 
 __author__ = 'rohe0002'
 
@@ -10,6 +11,18 @@ from oictest.check import factory as check_factory
 from oic.oic import Client
 from oic.oic.consumer import Consumer
 from oic.oic.message import factory as message_factory
+
+LOGGER = logging.getLogger("oic")
+LOGFILE_NAME = 'oictest.log'
+
+hdlr = logging.FileHandler(LOGFILE_NAME)
+
+base_formatter = logging.Formatter(
+    "%(asctime)s %(name)s:%(levelname)s %(message)s")
+
+hdlr.setFormatter(base_formatter)
+LOGGER.addHandler(hdlr)
+LOGGER.setLevel(logging.DEBUG)
 
 cli = OIC(oic_operations, Client, Consumer, message_factory, check_factory,
           Conversation)
