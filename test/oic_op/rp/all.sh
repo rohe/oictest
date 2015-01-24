614eaac0710d5758ea76f5f7741ce67a402553d9
Roland Hedberg <roland.hedberg@adm.umu.se>
2015-01-24 15:57:33 +0100

    Removed 2 of 3 instances for adfs and azure_ad. Added 2 more for Thinktecture.

--- test/oic_op/rp/all.sh
+++ test/oic_op/rp/all.sh
@@ -2,18 +2,16 @@
 
 startme() {
     ./oprp2.py -p C.T.F -t tflow adfs &> adfs.err &
-    ./oprp2.py -p I.T.F -t tflow adfs_i &> adfs_i.err &
-    ./oprp2.py -p IT.T.F -t tflow adfs_it &> adfs_it.err &
-
     ./oprp2.py -p C.T.F -t tflow azure_ad &> azure.err &
-    ./oprp2.py -p I.T.F -t tflow azure_ad_i &> azure_i.err &
-    ./oprp2.py -p IT.T.F -t tflow azure_ad_it &> azure_it.err &
 
     ./oprp2.py -p C.T.F -t tflow google &> google.err &
     ./oprp2.py -p C.T.F -t tflow ping &> ping.err &
     ./oprp2.py -p C.T.F -t tflow salesforce &> salesforce.err &
     ./oprp2.py -p C.T.F -t tflow telekom &> telekom.err &
+
     ./oprp2.py -p C.T.F -t tflow thinktecture_code &> thinktecture_code.err &
+    ./oprp2.py -p IT.T.F -t tflow thinktecture_impl &> thinktecture_impl.err &
+    ./oprp2.py -p CIT.T.F -t tflow thinktecture_hybr &> thinktecture_hybr.err &
 
     ./oprp2.py -p C.T.T -t tflow luke &> luke.err &
     ./oprp2.py -p C.T.T -t tflow edmund &> edmund.err &
@@ -27,7 +25,7 @@ stopme() {
     pkill -f "ping"
     pkill -f "salesforce"
     pkill -f "telekom"
-    pkill -f "thinktecture_code"
+    pkill -f "thinktecture"
     pkill -f "luke"
     pkill -f "edmund"
     pkill -f "xenosmilus2"
