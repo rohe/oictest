c3c980766f83985aea33865b8a9e34d02985dff4
daniel evertsson <daniel.evertsson@umu.se>
2015-03-16 15:40:00 +0100

    Made it possible to start up tp five different OPRP instances for a single issuer.

--- test/oic_op/config_server/test_config_server.py
+++ test/oic_op/config_server/test_config_server.py
@@ -1,9 +1,12 @@
 import subprocess
 from config_server import NoResponseException
 from config_server import check_if_oprp_started
-from config_server import write_config_file
 from config_server import kill_existing_process_on_port
 from config_server import get_oprp_pid
+from config_server import convert_to_simple_list
+from config_server import convert_to_gui_list
+from config_server import create_new_configuration_dict
+from config_server import get_issuer_from_gui_config
 
 __author__ = 'danielevertsson'
 
@@ -14,10 +17,6 @@ class TestConfigServer(unittest.TestCase):
     def test_check_if_oprp_started_raises_NoResponseException(self):
         with self.assertRaises(NoResponseException):
             check_if_oprp_started(None, oprp_url="http://1234.1234.1234.1234:8000", timeout=1)
-
-    def test_write_config_without_write_access(self):
-        with self.assertRaises(IOError):
-            write_config_file("no_write_access_file.txt", "content")
 
     def test_returns_correct_pid(self):
         p = subprocess.Popen(['grep', 'rp_conf_0.py'], stdout=subprocess.PIPE)
@@ -33,6 +32,30 @@ class TestConfigServer(unittest.TestCase):
         kill_existing_process_on_port(_port)
         _pid = get_oprp_pid(_port)
         self.assertEqual(_pid, None)
+
+    def test_convert_to_simple_list(self):
+        gui_list = [{"name": "test1"}, {"name": "test2"}]
+        simple_list = convert_to_simple_list(gui_list)
+        self.assertEqual(["test1", "test2"], simple_list)
+
+    def test_convert_to_gui_list(self):
+        gui_list = convert_to_gui_list(["test1", "test2"])
+        expected_list = [{"type": "test1", "name": "test1"},
+                         {"type": "test2", "name": "test2"}]
+        self.assertEqual(gui_list, expected_list)
+
+    def test_get_static_disco_issuer_from_gui_config(self):
+        issuer = 'issuer_test'
+        gui_config = create_new_configuration_dict(issuer)
+        returned_issuer = get_issuer_from_gui_config(gui_config)
+        self.assertEqual(issuer, returned_issuer)
+
+    def test_get_dynamic_disco_issuer_from_gui_config(self):
+        issuer = 'issuer_test'
+        gui_config = create_new_configuration_dict()
+        gui_config['fetchDynamicInfoFromServer']['input_field']['value'] = issuer
+        returned_issuer = get_issuer_from_gui_config(gui_config)
+        self.assertEqual(issuer, returned_issuer)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ test/oic_op/config_server/test_port_issuer_instance_id_database.py
@@ -0,0 +1,167 @@
+import os
+from port_issuer_instance_id_database import PortDatabase
+from port_issuer_instance_id_database import NoPortAvailable
+
+__author__ = 'danielevertsson'
+
+import unittest
+import dataset
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    TABLE_NAME = 'ports'
+
+    def setUp(self):
+        self.test_db = "./test.db"
+        self.database = PortDatabase(self.test_db)
+
+    def tearDown(self):
+        os.remove(self.test_db)
+
+    def create_three_unique_entries(self):
+        self.database._upsert(port=8001, issuer="google", instance_id='test1', port_type="dynamic")
+        self.database._upsert(port=8002, issuer="facebook", instance_id='test2', port_type="static")
+        self.database._upsert(port=8003, issuer="apberget", instance_id='test3', port_type="static")
+
+    def test_list_all_ports(self):
+        self.create_three_unique_entries()
+        ports = self.database.get_all_ports()
+        self.assertEqual(ports, [8001, 8002, 8003])
+
+    def test_list_all_issuers(self):
+        self.create_three_unique_entries()
+        issuers = self.database.get_all_issuers()
+        self.assertEqual(issuers, ["google", "facebook", "apberget"])
+
+    def test_get_port_by_issuer(self):
+        port = 8001
+        self.create_three_unique_entries()
+        row = self.database.table.find_one(port=port)
+        self.assertEqual(row['port'], port)
+
+    def test_get_database_as_list_and_check_number_of_elements(self):
+        self.create_three_unique_entries()
+        list = self.database.get_table_as_list()
+        self.assertEqual(len(list), 3)
+
+    def test_get_database_as_list_and_check_if_ports_are_correct(self):
+        self.create_three_unique_entries()
+        list = self.database.get_table_as_list()
+        ports = []
+        for element in list:
+            ports.append(element[0])
+        self.assertEqual(ports, [8001, 8002, 8003])
+
+    def test_print_table(self):
+        self.create_three_unique_entries()
+        self.database.print_table()
+
+    def create_three_entries_with_same_port(self):
+        self.database._upsert(port=8001, issuer="google", instance_id='test1', port_type="dynamic")
+        self.database._upsert(port=8001, issuer="facebook", instance_id='test2', port_type="static")
+        self.database._upsert(port=8001, issuer="apberget", instance_id='test3', port_type="static")
+
+    def test_if_entries_with_same_port_is_only_updated(self):
+        self.create_three_entries_with_same_port()
+        ports = self.database.get_all_ports()
+        self.assertEqual(ports, [8001])
+
+    def test_remove_entry_based_on_port(self):
+        self.create_three_unique_entries()
+        self.database._remove_row(8001)
+        self.assertEqual(self.database.get_all_ports(), [8002, 8003])
+
+    def create_three_entries_with_same_issuer_google_but_different_instance_ids(self):
+        self.database._upsert(port=8001, issuer="google", instance_id='test1', port_type="dynamic")
+        self.database._upsert(port=8002, issuer="google", instance_id='test2', port_type="static")
+        self.database._upsert(port=8003, issuer="google", instance_id='test3', port_type="static")
+
+    def test_get_port_based_on_issuer_and_id(self):
+        self.create_three_entries_with_same_issuer_google_but_different_instance_ids()
+        port = self.database._get_existing_port("google", 'test2', "static")
+        self.assertEqual(port, 8002)
+
+    def test_get_next_free_port(self):
+        self.create_three_entries_with_same_issuer_google_but_different_instance_ids()
+        port = self.database._get_next_free_port(8001, 8010)
+        self.assertEqual(port, 8004)
+
+    def test_enter_row_with_existing_port(self):
+        self.create_three_entries_with_same_issuer_google_but_different_instance_ids()
+        port = self.database.enter_row("google", 'test2', "static", 8001, 8010)
+        self.assertEqual(port, 8002)
+
+    def test_enter_row_with_non_existing_port(self):
+        self.create_three_entries_with_same_issuer_google_but_different_instance_ids()
+        port = self.database.enter_row("google", 'test4', "static", 8001, 8010)
+        self.assertEqual(port, 8004)
+
+    ISSUER_GOOGLE = "google"
+    DYNAMIC_CLIENT_REGISTRATION_PORT_RANGE_MIN = 1
+    DYNAMIC_CLIENT_REGISTRATION_PORT_RANGE_MAX = 3
+
+    def allocate_ports(self, number_of_entries, port_type, issuer=None):
+        if not issuer:
+            issuer = self.ISSUER_GOOGLE
+
+        index = 0
+        while index < number_of_entries:
+            index += 1
+            port = self.database.enter_row(issuer,
+                                           "ID_%s" % index,
+                                           port_type,
+                                           self.DYNAMIC_CLIENT_REGISTRATION_PORT_RANGE_MIN,
+                                           self.DYNAMIC_CLIENT_REGISTRATION_PORT_RANGE_MAX)
+        return port
+
+    def test_fill_port_database(self):
+        port = self.allocate_ports(self.DYNAMIC_CLIENT_REGISTRATION_PORT_RANGE_MAX,
+                                   self.database.DYNAMIC_PORT_TYPE)
+        self.assertEqual(port, self.DYNAMIC_CLIENT_REGISTRATION_PORT_RANGE_MAX)
+
+    def test_add_to_many_entries_to_port_database(self):
+        with self.assertRaises(NoPortAvailable):
+            self.allocate_ports(self.DYNAMIC_CLIENT_REGISTRATION_PORT_RANGE_MAX+1,
+                                       self.database.DYNAMIC_PORT_TYPE)
+
+    def test_get_existing_port_with_non_existing_credientials(self):
+        port = self.database._get_existing_port("","","")
+        self.assertEqual(port, None)
+
+    def test_get_port_type(self):
+        self.database._upsert(port=8001, issuer="google", instance_id='test1', port_type=PortDatabase.DYNAMIC_PORT_TYPE)
+        port_type = self.database._get_port_type(8001)
+        self.assertEqual(port_type, PortDatabase.DYNAMIC_PORT_TYPE)
+
+    def test_allocate_dynamic_port_do_not_use_existing_static_port(self):
+        issuer = "google"
+        instance_id='test1'
+        self.database._upsert(port=8501, issuer=issuer, instance_id=instance_id, port_type=PortDatabase.STATIC_PORT_TYPE)
+        port = self.database.enter_row(issuer, instance_id, PortDatabase.DYNAMIC_PORT_TYPE, 8001, 8003)
+        self.assertEqual(port, 8001)
+
+    def test_use_existing_dynamic_port(self):
+        issuer = "google"
+        instance_id='test1'
+        self.database._upsert(port=8001, issuer=issuer, instance_id=instance_id, port_type=PortDatabase.STATIC_PORT_TYPE)
+        port = self.database.enter_row(issuer, instance_id, PortDatabase.STATIC_PORT_TYPE, 8001, 8003)
+        self.assertEqual(port, 8001)
+
+    def test_if_static_port_is_remove_when_switching_to_dynamic_port(self):
+        issuer = "google"
+        instance_id='test1'
+        static_port = 8501
+        self.database._upsert(port=static_port, issuer=issuer, instance_id=instance_id, port_type=PortDatabase.STATIC_PORT_TYPE)
+        self.database.enter_row(issuer, instance_id, PortDatabase.DYNAMIC_PORT_TYPE, 8001, 8003)
+        ports = self.database.get_all_ports()
+        self.assertNotIn(static_port, ports)
+
+    def test_list_instance_ids_for_one_issuer(self):
+        self.allocate_ports(3, PortDatabase.DYNAMIC_PORT_TYPE, issuer=self.ISSUER_GOOGLE)
+        self.database._upsert(port=8004, issuer="apberget", instance_id='test1', port_type="static")
+        self.database._upsert(port=8005, issuer="apberget", instance_id='test2', port_type="static")
+        instance_ids = self.database.get_instance_ids(self.ISSUER_GOOGLE)
+        self.assertEqual(instance_ids, ["ID_1", "ID_2", "ID_3"])
+
+if __name__ == '__main__':
+    unittest.main()
