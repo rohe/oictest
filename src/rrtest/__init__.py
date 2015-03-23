85be8e16405e81d04d7ec82722efa85f7e59300d
Roland Hedberg <roland.hedberg@adm.umu.se>
2015-03-23 10:12:24 +0100

    If not an exception don't print trace info for the last exception.

--- src/rrtest/__init__.py
+++ src/rrtest/__init__.py
@@ -155,10 +155,12 @@ def get_page(url):
 def exception_trace(tag, exc, log=None):
     message = traceback.format_exception(*sys.exc_info())
     if log:
-        log.error("[%s] ExcList: %s" % (tag, "".join(message),))
+        if isinstance(exc, Exception):
+            log.error("[%s] ExcList: %s" % (tag, "".join(message),))
         log.error("[%s] Exception: %s" % (tag, exc))
     else:
-        print >> sys.stderr, "[%s] ExcList: %s" % (tag, "".join(message),)
+        if isinstance(exc, Exception):
+            print >> sys.stderr, "[%s] ExcList: %s" % (tag, "".join(message),)
         try:
             print >> sys.stderr, "[%s] Exception: %s" % (tag, exc)
         except UnicodeEncodeError:
