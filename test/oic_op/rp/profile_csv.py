b4afc2791b7a5e48faff7b51498fa746c2dac27f
Roland Hedberg <roland.hedberg@adm.umu.se>
2015-04-06 14:49:16 -0700

    More info

--- test/oic_op/rp/profile_csv.py
+++ test/oic_op/rp/profile_csv.py
@@ -33,7 +33,7 @@ PROF = {
 
 ISSUERS = {
     u'https%3A%2F%2Fwww.paypal.com': "paypal",
-    u'https%3A%2F%2Fstsadweb.one.microsoft.com%2Fadfs': "AD",
+    u'https%3A%2F%2Fstsadweb.one.microsoft.com%2Fadfs': "ADFS",
     u'https%3A%2F%2Fsts.windows.net%2Fb4ea3de6-839e-4ad1-ae78-c78e5c0cdc06%2F': "Azure",
     u'https%3A%2F%2Fseed.gluu.org': "gluu",
     u'https%3A%2F%2Foidcp.openrock.org%3A8043%2Fopenam%2Foauth2': "openrock",
@@ -49,6 +49,39 @@ ISSUERS = {
     u'https%3A%2F%2Flogin.salesforce.com': "salesforce",
     "https%3A%2F%2Foictest.umdc.umu.se%3A8060%2F": "umu"
 }
+
+EXTRAS = [
+    "OP-ClientAuth-PrivateJWT",
+    "OP-ClientAuth-SecretJWT",
+    "OP-Discovery-WebFinger",
+    "OP-Discovery-WebFinger-Email",
+    "OP-IDToken-ES256",
+    "OP-IDToken-HS256",
+    "OP-IDToken-SigEnc",
+    "OP-Registration-Read",
+    "OP-Registration-Sub-Differ",
+    "OP-Registration-Sub-Pairwise",
+    "OP-Registration-Sub-Public",
+    "OP-Response-form_post",
+    "OP-Rotation-RP-Enc",
+    "OP-UserInfo-Enc",
+    "OP-UserInfo-SigEnc",
+    "OP-claims-Combined",
+    "OP-claims-IDToken",
+    "OP-claims-Split",
+    "OP-claims-acr-essential",
+    "OP-claims-acr-voluntary",
+    "OP-claims-acr=1",
+    "OP-claims-auth_time-essential",
+    "OP-claims-essential+voluntary",
+    "OP-claims-sub",
+    "OP-claims-voluntary",
+    "OP-redirect_uri-MissingOK",
+    "OP-request-Sig",
+    "OP-request-Support",
+    "OP-request_uri-Enc",
+    "OP-request_uri-SigEnc"
+]
 
 
 def do_profile(prof, tests, issuers, inp):
